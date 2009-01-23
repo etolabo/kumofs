2e9de4f9df4385c770a0b7007247380a75d588b5
frsyuki <frsyuki@b300df90-00d7-44b1-9271-3fa3ef26909a>
2009-01-23 18:21:33 +0000

    fix kumomergedb
    git-svn-id: svn+ssh://code.etolabo.org/usr/etolabo/var/svn/ficia/kumofs/trunk@638 b300df90-00d7-44b1-9271-3fa3ef26909a

--- logic/mergedb.cc
+++ logic/mergedb.cc
@@ -18,44 +18,53 @@ private:
 int main(int argc, char* argv[])
 {
 	if(argc <= 3) {
-		std::cerr << "usage: "<<argv[0]<<" <dst> <src.tch>..." << std::endl;
+		std::cerr << "usage: "<<argv[0]<<" <dst.tch> <src.tch>..." << std::endl;
 		return 1;
 	}
 
+	mlogger::reset(new mlogger_ostream(mlogger::TRACE, std::cout));
+
+	const char* dst = argv[1];
+	unsigned int nsrc = argc - 2;
+	char* const* psrc = argv + 2;
 	using namespace kumo;
-
-	unsigned int nsrc = argc - 3;
-	char** psrc = argv + 2;
-
-	std::auto_ptr<Storage> dst(new Storage(argv[1]));
-
-	auto_array< std::auto_ptr<Storage> > srcs(new std::auto_ptr<Storage>[nsrc]);
-	for(unsigned int i=0; i < nsrc; ++i) {
-		srcs[i].reset(new Storage(psrc[i]));
-	}
-
-	for(unsigned int i=0; i < nsrc; ++i) {
-
-		Storage::iterator kv;
-		while(srcs[i]->iterator_next(kv)) {
-			if(kv.keylen() < DBFormat::VALUE_META_SIZE) { continue; }
-			if(kv.vallen() < DBFormat::KEY_META_SIZE) { continue; }
-
-			protocol::type::DBKey key(kv.key(), kv.keylen());
-			protocol::type::DBValue val(kv.val(), kv.vallen());
-
-			uint64_t clocktime = 0;
-			bool stored = DBFormat::get_clocktime(*dst,
-					key.raw_data(), key.raw_size(), &clocktime);
-
-			if(!stored || ClockTime(clocktime) <= ClockTime(val.clocktime())) {
-				// stored key is old
-				dst->set_async(
-						key.raw_data(), key.raw_size(),
-						val.raw_data(), val.raw_size());
+	{
+		auto_array< std::auto_ptr<Storage> > srcs(new std::auto_ptr<Storage>[nsrc]);
+		for(unsigned int i=0; i < nsrc; ++i) {
+			srcs[i].reset(new Storage(psrc[i]));
+		}
+	
+		std::auto_ptr<Storage> db(new Storage(dst));
+	
+		for(unsigned int i=0; i < nsrc; ++i) {
+	
+			std::cout << "merging "<<psrc[i]<< "..." << std::endl;
+			Storage::iterator kv;
+			srcs[i]->iterator_init(kv);
+			while(srcs[i]->iterator_next(kv)) {
+				if(kv.keylen() < DBFormat::KEY_META_SIZE) { continue; }
+				if(kv.vallen() < DBFormat::VALUE_META_SIZE) { continue; }
+	
+				protocol::type::DBKey key(kv.key(), kv.keylen());
+				protocol::type::DBValue val(kv.val(), kv.vallen());
+	
+				uint64_t clocktime = 0;
+				bool stored = DBFormat::get_clocktime(*db,
+						key.raw_data(), key.raw_size(), &clocktime);
+	
+				if(!stored || ClockTime(clocktime) < ClockTime(val.clocktime())) {
+					db->set_async(
+							key.raw_data(), key.raw_size(),
+							val.raw_data(), val.raw_size());
+				}
 			}
+	
 		}
 
+		std::cout << "closing "<<dst<<"..." << std::endl;
 	}
+
+	std::cout << "done." << std::endl;
+	return 0;
 }
 
