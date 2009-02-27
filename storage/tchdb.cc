a4118cef3e63404ebf53a51e54010cdb3b2fbe89
frsyuki <frsyuki@b300df90-00d7-44b1-9271-3fa3ef26909a>
2009-02-27 15:24:38 +0000

    tchdb: C -> C++
    git-svn-id: svn+ssh://code.etolabo.org/usr/etolabo/var/svn/ficia/kumofs/trunk@909 b300df90-00d7-44b1-9271-3fa3ef26909a

--- storage/tchdb.cc
+++ storage/tchdb.cc
@@ -1,52 +1,68 @@
 #include "storage.h"  // FIXME
 #include <tchdb.h>
+#include <mp/pthread.h>
 
-//typedef struct {
-//	TCHDB* db;
-//	TCLIST* garbage;
-//} kumo_tchdb_ctx;
+struct kumo_tchdb {
+	kumo_tchdb()
+	{
+		db = tchdbnew();
+		if(!db) {
+			throw std::bad_alloc();
+		}
+
+		garbage = kumo_buffer_queue_new();
+		if(!garbage) {
+			tchdbdel(db);
+			throw std::bad_alloc();
+		}
+	}
+
+	~kumo_tchdb()
+	{
+		tchdbdel(db);
+		kumo_buffer_queue_free(garbage);
+	}
+
+	TCHDB* db;
+	kumo_buffer_queue* garbage;
+	mp::pthread_mutex garbage_mutex;
+};
 
 
 static void* kumo_tchdb_create(void)
-{
-	TCHDB* db = tchdbnew();
-	return (void*)db;
+try {
+	kumo_tchdb* ctx = new kumo_tchdb();
+	return reinterpret_cast<void*>(ctx);
+
+} catch (...) {
+	return NULL;
 }
 
 static void kumo_tchdb_free(void* data)
 {
-	TCHDB* db = (TCHDB*)data;
-	tchdbdel(db);
+	kumo_tchdb* ctx = reinterpret_cast<kumo_tchdb*>(data);
+	delete ctx;
 }
 
-//static bool kumo_tchdb_open(void* data, int* argc, char** argv)
 static bool kumo_tchdb_open(void* data, const char* path)
 {
-	TCHDB* db = (TCHDB*)data;
+	kumo_tchdb* ctx = reinterpret_cast<kumo_tchdb*>(data);
 
-	//const char* path = argv[1];
-	//*argc = 0;
-
-	if(!tchdbsetmutex(db)) {
+	if(!tchdbsetmutex(ctx->db)) {
 		return false;
 	}
 
-	if(!tchdbopen(db, path, HDBOWRITER|HDBOCREAT)) {
+	if(!tchdbopen(ctx->db, path, HDBOWRITER|HDBOCREAT)) {
 		return false;
 	}
-
-	/* FIXME
-	struct timespec interval = { 24*60*60, 0 };
-	kumo_start_timer(&interval, kumo_tchdb_timer, (void*)db);
-	*/
 
 	return true;
 }
 
 static void kumo_tchdb_close(void* data)
 {
-	TCHDB* db = (TCHDB*)data;
-	tchdbclose(db);
+	kumo_tchdb* ctx = reinterpret_cast<kumo_tchdb*>(data);
+	tchdbclose(ctx->db);
 }
 
 
@@ -55,10 +71,10 @@ static const char* kumo_tchdb_get(void* data,
 		uint32_t* result_vallen,
 		msgpack_zone* zone)
 {
-	TCHDB* db = (TCHDB*)data;
+	kumo_tchdb* ctx = reinterpret_cast<kumo_tchdb*>(data);
 
 	int len;
-	char* val = tchdbget(db, key, keylen, &len);
+	char* val = (char*)tchdbget(ctx->db, key, keylen, &len);
 	if(!val) {
 		return NULL;
 	}
@@ -73,32 +89,26 @@ static const char* kumo_tchdb_get(void* data,
 }
 
 
-//static void kumo_tchdb_timer(void* data)
-//{
-//	// FIXME garbage collect
-//}
-
-
 typedef struct {
-	const void* val;
+	const char* val;
 	uint32_t vallen;
 } kumo_tchdb_update_ctx;
 
 static void* kumo_tchdb_update_proc(const void* vbuf, int vsiz, int *sp, void* op)
 {
-	kumo_tchdb_update_ctx* ctx = (kumo_tchdb_update_ctx*)op;
+	kumo_tchdb_update_ctx* upctx = (kumo_tchdb_update_ctx*)op;
 
 	if(vsiz < 8 || kumo_clocktime_less(
-			kumo_storage_clocktime_of(vbuf),
-			kumo_storage_clocktime_of(ctx->val))) {
+			kumo_storage_clocktime_of((const char*)vbuf),
+			kumo_storage_clocktime_of(upctx->val))) {
 
-		void* mem = malloc(ctx->vallen);
+		void* mem = malloc(upctx->vallen);
 		if(!mem) {
 			return NULL;  // FIXME
 		}
 
-		*sp = ctx->vallen;
-		memcpy(mem, ctx->val, ctx->vallen);
+		*sp = upctx->vallen;
+		memcpy(mem, upctx->val, upctx->vallen);
 		return mem;
 
 	} else {
@@ -111,64 +121,65 @@ static bool kumo_tchdb_set(void* data,
 		const char* key, uint32_t keylen,
 		const char* val, uint32_t vallen)
 {
-	TCHDB* db = (TCHDB*)data;
-	return tchdbput(db, key, keylen, val, vallen);
+	kumo_tchdb* ctx = reinterpret_cast<kumo_tchdb*>(data);
+	return tchdbput(ctx->db, key, keylen, val, vallen);
 }
 
 static bool kumo_tchdb_update(void* data,
 		const char* key, uint32_t keylen,
 		const char* val, uint32_t vallen)
 {
-	TCHDB* db = (TCHDB*)data;
+	kumo_tchdb* ctx = reinterpret_cast<kumo_tchdb*>(data);
 
-	kumo_tchdb_update_ctx ctx = { val, vallen };
+	kumo_tchdb_update_ctx upctx = { val, vallen };
 
-	return tchdbputproc(db,
+	return tchdbputproc(ctx->db,
 			key, keylen,
 			val, vallen,
-			kumo_tchdb_update_proc, &ctx);
+			kumo_tchdb_update_proc, &upctx);
 }
 
 static bool kumo_tchdb_del(void* data,
 		const char* key, uint32_t keylen,
 		uint64_t clocktime)
 {
-	TCHDB* db = (TCHDB*)data;
+	kumo_tchdb* ctx = reinterpret_cast<kumo_tchdb*>(data);
 
 	char clockbuf[8];
 	kumo_storage_clocktime_to(clocktime, clockbuf);
 
-	kumo_tchdb_update_ctx ctx = { clockbuf, 8 };
+	kumo_tchdb_update_ctx upctx = { clockbuf, 8 };
 
-	return tchdbputproc(db,
+	// FIXME push the key to ctx->garbage
+	return tchdbputproc(ctx->db,
 			key, keylen,
 			clockbuf, 8,
-			kumo_tchdb_update_proc, &ctx);
+			kumo_tchdb_update_proc, &upctx);
 }
 
 static uint64_t kumo_tchdb_rnum(void* data)
 {
-	TCHDB* db = (TCHDB*)data;
-	return tchdbrnum(db);
+	kumo_tchdb* ctx = reinterpret_cast<kumo_tchdb*>(data);
+	return tchdbrnum(ctx->db);
 }
 
 static bool kumo_tchdb_backup(void* data, const char* dstpath)
 {
-	TCHDB* db = (TCHDB*)data;
-	return tchdbcopy(db, dstpath);
+	kumo_tchdb* ctx = reinterpret_cast<kumo_tchdb*>(data);
+	return tchdbcopy(ctx->db, dstpath);
 }
 
 static const char* kumo_tchdb_error(void* data)
 {
-	TCHDB* db = (TCHDB*)data;
-	return tchdberrmsg(tchdbecode(db));
+	kumo_tchdb* ctx = reinterpret_cast<kumo_tchdb*>(data);
+	return tchdberrmsg(tchdbecode(ctx->db));
 }
 
 
 typedef struct {
 	TCXSTR* key;
 	TCXSTR* val;
-	TCHDB* db;
+	kumo_tchdb* ctx;
 } kumo_tchdb_iterator;
 
 static int kumo_tchdb_for_each(void* data,
@@ -176,18 +187,18 @@ static int kumo_tchdb_for_each(void* data,
 {
 	// FIXME only one thread can use iterator. use mutex.
 
-	TCHDB* db = (TCHDB*)data;
+	kumo_tchdb* ctx = reinterpret_cast<kumo_tchdb*>(data);
 
-	if(!tchdbiterinit(db)) {
+	if(!tchdbiterinit(ctx->db)) {
 		return -1;
 	}
 
 	kumo_tchdb_iterator it = { NULL, NULL, NULL };
 	it.key = tcxstrnew(); if(!it.key) { return -1; }
 	it.val = tcxstrnew(); if(!it.val) { tcxstrdel(it.key); return -1; }
-	it.db  = db;
+	it.ctx  = ctx;
 
-	while( tchdbiternext3(db, it.key, it.val) ) {
+	while( tchdbiternext3(ctx->db, it.key, it.val) ) {
 		if(TCXSTRSIZE(it.val) < 16 || TCXSTRSIZE(it.key) < 8) {
 			// FIXME delete it?
 			continue;
@@ -278,7 +289,7 @@ static bool kumo_tchdb_iterator_delete(void* iterator_data)
 	const char* key = TCXSTRPTR(it->key);
 	size_t keylen = TCXSTRSIZE(it->key);
 
-	return tchdbout(it->db, key, keylen);
+	return tchdbout(it->ctx->db, key, keylen);
 }
 
 static bool kumo_tchdb_iterator_delete_if_older(void* iterator_data, uint64_t if_older)
@@ -295,7 +306,7 @@ static bool kumo_tchdb_iterator_delete_if_older(void* iterator_data, uint64_t if
 		const char* key = TCXSTRPTR(it->key);
 		size_t keylen = TCXSTRSIZE(it->key);
 
-		return tchdbout(it->db, key, keylen);
+		return tchdbout(it->ctx->db, key, keylen);
 	}
 
 	return false;
