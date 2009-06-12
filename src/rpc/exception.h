0898f4086b833a799a866f5f6314bc15f7aa20c6
frsyuki <frsyuki@b300df90-00d7-44b1-9271-3fa3ef26909a>
2009-06-12 09:43:50 +0000

    src/rpc/exception.h
    git-svn-id: svn+ssh://code.etolabo.org/usr/etolabo/var/svn/ficia/kumofs/trunk@1781 b300df90-00d7-44b1-9271-3fa3ef26909a

---
+++ src/rpc/exception.h
@@ -0,0 +1,28 @@
+#ifndef RPC_EXCEPTION_H__
+#define RPC_EXCEPTION_H__
+
+#include <exception>
+
+namespace rpc {
+
+
+struct connection_error : public std::exception {
+	connection_error() { }
+};
+
+struct connection_closed_error : public connection_error {
+	connection_closed_error() { }
+	const char* what() const throw() { return "connection closed"; }
+};
+
+struct connection_broken_error : public connection_error {
+	connection_broken_error() { }
+	const char* what() const throw() { return "connection broken"; }
+};
+
+
+
+}  // namespace rpc
+
+#endif /* rpc/connection.h */
+
